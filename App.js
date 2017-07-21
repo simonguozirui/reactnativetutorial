25b7f45413774054822e8afc63395c52797b1e53
simonguozirui <simonguozirui@gmail.com>
2017-07-20 23:29:22 -0400

    hello world

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Text>Hello world!</Text>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
